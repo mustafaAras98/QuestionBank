dfa1dab556cd094834c2122e01cf180c8bff0aba
mustafaAras98 <mustafa_aras_25@hotmail.com>
2024-12-12 19:40:32 +0300

    SignIn/SignUp assigned as screen
    SignIn/SignUp assigned as screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {SafeAreaView, View, ImageBackground, Dimensions} from 'react-native';
 
 import SafeViewAndroid from './Utils/SafeViewAndroid.style';
 import {styles} from './Utils/App.style';
 
-import SignIn from './Components/SignIn';
-import SignUp from './Components/SignUp';
+import SignIn from './Screens/SignIn';
+import SignUp from './Screens/SignUp';
 
 import BackgroundImage from './Assets/BackgroundImage.png';
 

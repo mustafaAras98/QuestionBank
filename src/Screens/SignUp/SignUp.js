b5828267e8be8a4869dcd7260d2b6f14626727be
mustafaAras98 <mustafa_aras_25@hotmail.com>
2024-12-17 10:08:34 +0300

    TextInputComp created and implemented
    Text Inputlar için Component oluşturuldu. SignUp ve SignIn sayfalarına implemente edildi. Ayrıca Firebase kullanımı için bazı dosyalar hazır edildi.

--- src/Screens/SignUp/SignUp.js
+++ src/Screens/SignUp/SignUp.js
@@ -1,21 +1,20 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 
 import {styles} from './SignUp.style';
-import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
 
 import BackgroundContainer from '../../Components/BackgroundContainerComponent';
 import ValidateEmailSchema from '../../Utils/Validation/ValidateEmailSchema';
 import ValidateUsernameSchema from '../../Utils/Validation/ValidateUsernameSchema';
 import ValidatePasswordSchema from '../../Utils/Validation/ValidatePasswordSchema';
+import TextInputComp from '../../Components/TextInputComp';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
   const [message, setMessage] = useState('');
 
   const onSubmitPress = () => {
@@ -23,13 +22,13 @@ const SignUp = () => {
     let usernameValidate = ValidateUsernameSchema(username.trim(), 8, 18);
     let passwordValidate = ValidatePasswordSchema(password.trim(), 8, 18);
 
-    emailValidate && usernameValidate === 'Valid' && passwordValidate === 'Valid'
+    emailValidate &&
+    usernameValidate === 'Valid' &&
+    passwordValidate === 'Valid'
       ? setMessage('Valid')
-      : setMessage(`${emailValidate}\n${usernameValidate}\n${passwordValidate}`);
-  };
-
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
+      : setMessage(
+          `${emailValidate}\n${usernameValidate}\n${passwordValidate}`,
+        );
   };
 
   const navigation = useNavigation();
@@ -38,57 +37,33 @@ const SignUp = () => {
       <View style={styles.Container}>
         <View style={styles.GlassBackground} />
         <View style={styles.RegisterFormContainer}>
-          <View style={styles.TextInputContainer}>
-            <Text style={styles.Label}>E-Mail</Text>
-            <FontAwesome6
-              style={styles.PromptIcon}
-              name="envelope"
-              iconStyle="solid"
-            />
-            <TextInput
-              placeholder="Enter Your E-Mail..."
-              onChangeText={setEmail}
-              value={email}
-              style={styles.InputComp}
-            />
-          </View>
-          <View style={styles.TextInputContainer}>
-            <Text style={styles.Label}>Username</Text>
-            <FontAwesome6
-              style={styles.PromptIcon}
-              name="user"
-              iconStyle="solid"
-            />
-            <TextInput
-              placeholder="Enter Your Username..."
-              onChangeText={setUsername}
-              value={username}
-              maxLength={18}
-              style={styles.InputComp}
-            />
-          </View>
-          <View style={styles.TextInputContainer}>
-            <Text style={styles.Label}>Password</Text>
-            <FontAwesome6
-              style={styles.PromptIcon}
-              name="lock"
-              iconStyle="solid"
-            />
-            <TextInput
-              placeholder="Enter Your Password..."
-              onChangeText={setPassword}
-              value={password}
-              maxLength={18}
-              secureTextEntry={showPassword}
-              style={styles.InputComp}
-            />
-            <FontAwesome6
-              onPress={toggleShowPassword}
-              style={styles.PasswordHideIcon}
-              iconStyle="solid"
-              name={showPassword ? 'eye-slash' : 'eye'}
-            />
-          </View>
+          <TextInputComp
+            label="E-Mail"
+            placeholder="Enter Your  E-Mail..."
+            leftLogoName="envelope"
+            onChangeValue={setEmail}
+            value={email}
+            theme="primary"
+          />
+          <TextInputComp
+            label="Username"
+            placeholder="Enter Your Username..."
+            leftLogoName="user"
+            onChangeValue={setUsername}
+            value={username}
+            maxLength={18}
+            theme="primary"
+          />
+          <TextInputComp
+            label="Password"
+            placeholder="Enter Your Password..."
+            leftLogoName="lock"
+            onChangeValue={setPassword}
+            value={password}
+            maxLength={18}
+            theme="primary"
+            isPassword={true}
+          />
           <TouchableOpacity style={styles.SubmitButton} onPress={onSubmitPress}>
             <Text style={styles.ButtonText}>REGISTER</Text>
           </TouchableOpacity>

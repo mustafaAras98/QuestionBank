ac067620db48a277035f4fc1ea9e30787d36a576
mustafaAras98 <mustafa_aras_25@hotmail.com>
2025-01-07 00:28:01 +0300

    ButtomTab'a Logout işlemi eklendi
    ButtomTab'a Logout işlemi eklendi

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,16 +1,12 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import BackgroundContainer from '../../Components/BackgroundContainerComponent';
 import {styles} from './Home.style';
 import {useSelector} from 'react-redux';
-import authService from '../../Services/Auth.Service';
 
 const Home = () => {
   const user = useSelector(state => state.user);
 
-  const handleLogout = () => {
-    authService.logout();
-  };
   return (
     <BackgroundContainer>
       <View style={styles.Container}>
@@ -18,17 +14,6 @@ const Home = () => {
           {user.info.username}
           {user.info.email}
         </Text>
-        <TouchableOpacity
-          onPress={handleLogout}
-          style={{
-            backgroundColor: 'white',
-            width: 100,
-            height: 100,
-            alignSelf: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text>SignOut</Text>
-        </TouchableOpacity>
       </View>
     </BackgroundContainer>
   );

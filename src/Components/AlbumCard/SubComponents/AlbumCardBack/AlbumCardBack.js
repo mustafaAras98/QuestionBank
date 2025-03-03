32e853f6f425dad5a679413ce54880730c55b83b
mustafaAras98 <mustafa_aras_25@hotmail.com>
2025-03-03 17:02:46 +0300

    update

--- src/Components/AlbumCard/SubComponents/AlbumCardBack/AlbumCardBack.js
+++ src/Components/AlbumCard/SubComponents/AlbumCardBack/AlbumCardBack.js
@@ -77,7 +77,7 @@ const AlbumCardBack = ({onLongPress, albumItem, reFetchAlbums, isFlipped}) => {
       const albumIdEncrpyted = await Encrypt(albumItem.Uid);
       const encryptedHex = urlSafeEncode.encodeUrlSafeBase64(albumIdEncrpyted);
 
-      const deepLink = `https://questionbankwebhost.web.app/${encryptedHex}/${Enums.OpenImageList.Album}`;
+      const deepLink = `https://questionbankwebhost.web.app/${Enums.OpenImageList.Album}/${encryptedHex}`;
 
       await Share.share({
         title: 'QuestionBank',
